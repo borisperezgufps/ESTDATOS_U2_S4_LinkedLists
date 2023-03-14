aa60e5b76d5058569f0791a8d8845f94fc92b6fe
Boris Perez <borisperezg@ufps.edu.co>
2023-03-14 18:31:30 -0500

    Ajustes menores

--- src/estdatos/linkedlists/MyLinkedList.java
+++ src/estdatos/linkedlists/MyLinkedList.java
@@ -1,232 +1,3 @@
-<<<<<<< HEAD
-package estdatos.linkedlists;
-
-public class MyLinkedList<E> {
-
-	// Ojo, el apuntador a la cabeza de la lista nunca
-	// debe moverse, de lo contrario perderá la referencia
-	// al primer elemento.
-	private Node<E> head;
-	private int size = 0; // Number of elements in the list
-
-	/**
-	 * Agrega un elemento de tipo E a la lista
-	 * @param e
-	 */
-	public void add(E element) {
-		if(head==null) {
-			// Si la lista está vacía, la cabeza será el nodo que se creará
-			head = new Node<E>(element); 
-			size++;
-			// Se incluye return para que finalice el método
-			return; 
-		}
-
-		// Y si no está vacía?
-		
-		// Variable temporal apuntando a la cabeza
-		Node<E> current = head;
-		
-		// Recorro la lista moviendo el apuntador
-		while(current.getNext()!=null) {
-			current = current.getNext();
-		}
-		
-		// Cuando se encuentra el último nodo, es decir, 
-		// el nodo sin un nodo siguiente, entonces 
-		// se crea el nuevo nodo asignándolo como current
-		current.setNext(new Node<E>(element));
-		size++;
-		
-	}
-	
-	/**
-	 * Este metodo se asume que hace parte de una lista ordenada.
-	 * con cuatro elementos de tipo entero.
-	 * @param element
-	 */
-	public void insertarNodo(Node<E> nodoAnterior, Node<E> nodo, Node<E> nodoNuevo) {
-		Node<E> current = null;
-		if(nodo==null)		
-			current = head;
-		else 
-			current = nodo;
-		if(!nodoNuevo.esMenor(current)) {
-			insertarNodo(current, current.getNext(), nodoNuevo);
-		}else {
-			nodoAnterior.setNext(nodoNuevo);
-			nodoNuevo.setNext(current);
-		}
-		size++;
-	}
-	
-	/**
-	 * Imprime la lista completa.
-	 * @return String con la información de los elementos contenidos
-	 */
-	public String print() {
-		// Se crea un objeto que guardará el resultado
-		// de la concatenación. Se podría haber usado un
-		// String también.
-		StringBuffer sb = new StringBuffer("[");
-		
-		Node<E> current = head;
-		for(int i=0;i<size;i++) {
-			sb.append(current.getData());
-			current = current.getNext();
-			if(current != null)
-				sb.append(", ");
-			else
-				sb.append("]");
-		}
-		
-		return sb.toString();
-	}
-	
-	public void imprimirRecursivo() {
-		imprimirRecursivo(head);
-	}
-	
-	private void imprimirRecursivo(Node<E> apuntador) {
-		
-		if(apuntador!=null) {
-			System.out.println(apuntador.getData());
-			imprimirRecursivo(apuntador.getNext());
-		}
-	}
-	
-	public void imprimirRecursivo(Node<E> apuntador, boolean esPrimeraVez) {
-		
-		if(esPrimeraVez)
-			apuntador = head;
-		
-		if(apuntador!=null) {
-			System.out.println(apuntador.getData());
-			imprimirRecursivo(apuntador.getNext(), false);
-			
-			
-		}
-	}
-	
-	
-	
-	
-	/**
-	 * Busca un elemento dentro de una lista, y lo elimina.
-	 */
-	public void remove(E element) {
-		// TODO Considerar los posibles casos
-		// 1) Si el elemento a borrar está en la cabeza
-		// 2) Si el elemento a borrar está en la mitad
-		// 3) Si el elemento a borrar está en la cola
-		
-		
-		// ---------------------------
-		// Se considera el caso (2)
-		// ---------------------------
-		
-		// Se pregunta si hay un elemento luego de la cabeza
-		if(head.getNext()!=null) {
-		
-			Node<E> previous = head;
-			
-			while(previous.getNext()!=null) {
-					
-				Node<E> toFind = previous.getNext();
-				
-				if(!element.equals(toFind.getData())){
-					previous = toFind;
-				}else {
-					Node<E> current = toFind.getNext();
-					previous.setNext(current);
-					size--;
-					return;
-					// ¿Qué pasa con la relación que queda del 
-					// nodo a eliminar y el nodo que le sigue? 
-				}
-								
-			}
-		
-		}	
-		
-		// ---------------------------
-		// Se considera el caso (1)
-		// ---------------------------
-		
-		
-		// ---------------------------
-		// Se considera el caso (3)
-		// ---------------------------
-		
-	}
-	
-	// *************************************
-	// METODOS PARA IMPLEMENTAR POR LOS
-	// ESTUDIANTES
-	// *************************************
-	
-	
-	/**
-	 * 
-	 * @param element
-	 * @param pos
-	 */
-	public void add(E element, int pos) {
-		
-	}
-	
-	/**
-	 * 
-	 * @param pos
-	 * @return
-	 */
-	public E remove(int pos) {
-	
-		
-		return null;
-	}
-	
-	/**
-	 * 
-	 */
-	public void clear() {
-		
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public boolean isEmpty() {
-		return false;
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public Node<E> getFirst(){
-		return null;
-	}
-	
-	/**
-	 * 
-	 * @param n1
-	 * @param n2
-	 */
-	public void exchangeNodes(Node<E> n1, Node<E> n2) {
-		
-	}
-
-}
-
-
-
-
-
-
-
-=======
 package estdatos.linkedlists;
 
 public class MyLinkedList<E> {
@@ -241,11 +12,6 @@ public class MyLinkedList<E> {
 		return size;
 	}
 	
-	public E get(int pos) {
-		
-		return null;
-	}
-	
 	/**
 	 * Agrega un elemento de tipo E a la lista
 	 * @param e
@@ -353,6 +119,12 @@ public class MyLinkedList<E> {
 	// METODOS PARA IMPLEMENTAR POR LOS
 	// ESTUDIANTES
 	// *************************************
+	
+	
+	public E get(int pos) {
+		
+		return null;
+	}
 	
 	
 	/**
@@ -371,43 +143,8 @@ public class MyLinkedList<E> {
 	 */
 	public E remove(int pos) throws Exception {
 	
-		Node<E> p = head;
 		
-		if(pos>=size) {
-			throw new Exception("Esa posición no existe");
-		}else if(pos==0) {
-			
-			head = p.getNext();
-			p.setNext(null);
-			size--;
-			
-		}else {
-		
-			int cont = 0;
-			
-			while(cont!=pos) {		
-				p = p.getNext();
-				cont++;
-			}
-			
-			// Se sale del while, porque tal vez el contador se igualo
-			// a la posición buscada.
-			
-			int contAnterior = 0;
-			Node<E> q = head;
-			
-			while(contAnterior<pos-1) {
-				q = q.getNext();
-				contAnterior++;
-			}
-			// Se sale del while cuando llegue antes de P
-			
-			q.setNext(p.getNext());
-			p.setNext(null);
-			size--;
-		}
-		
-		return p.getData();
+		return null;
 	}
 	
 	/**
@@ -452,10 +189,3 @@ public class MyLinkedList<E> {
 
 }
 
-
-
-
-
-
-
->>>>>>> 18f5cbedcabeff64e59dd0f8e61ef189b7cab8c3
--- src/estdatos/linkedlists/Node.java
+++ src/estdatos/linkedlists/Node.java
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-package estdatos.linkedlists;
-
-public class Node<E> {
-
-	private E data; 
-	private Node<E> next;
-	
-	public Node(E ele) {
-		data = ele;
-	}
-	
-	public void setNext(Node<E> next) {
-		this.next = next;
-	}
-	
-	public Node<E> getNext(){
-		return next;
-	}
-	
-	public E getData() {
-		return data;
-	}
-	
-	/**
-	 * Este metodo compara si el elemento actual es menor
-	 * al elemento pasado como parámetro.
-	 * @param element
-	 * @return
-	 */
-	public boolean esMenor(Node<E> element) {
-		boolean esMenor = false;
-		
-		Integer elementInteger = (int)element.getData();
-		Integer thisInteger = (int) this.data;
-		
-		if(thisInteger<elementInteger)
-			esMenor = true;
-		
-		return esMenor;
-	}
-	
-}
-=======
 package estdatos.linkedlists;
 
 public class Node<E> {
@@ -67,4 +23,3 @@ public class Node<E> {
 
 	
 }
->>>>>>> 18f5cbedcabeff64e59dd0f8e61ef189b7cab8c3
